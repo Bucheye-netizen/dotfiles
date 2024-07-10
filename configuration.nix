291cebfdf738b7664c571dcc44e93eb5cfa4d4a5
Lisan <lisan.kontra@gmail.com>
2024-07-10 14:47:04 -0400

    Temporarily reverted to KDE while working through driver issues

--- configuration.nix
+++ configuration.nix
@@ -11,11 +11,13 @@
       inputs.home-manager.nixosModules.default
     ];
 
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; 
-  networking.networkmanager.enable = true;
+  # networking.networkmanager.enable = true;
+
   time.timeZone = "America/New_York";
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
@@ -30,15 +32,32 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  environment.sessionVariables = {
+    WLR_NO_HARDWARE = "1";
+    NIXOS_OZONE_WL = "1";
+  };
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+
+  hardware.graphics.enable = true;
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 
   services.xserver = {
     layout = "us";
     xkbVariant = "";
+    enable = true;
+    displayManager.sddm.wayland.enable = true;
   };
+  services.desktopManager.plasma6.enable = true;
   services.printing.enable = true;
+
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -47,22 +66,7 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
-
-  users.users.lisan = {
-    isNormalUser = true;
-    description = "Lisan";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      kdePackages.kate
-    ];
-  };
-
-  home-manager = {
-    extraSpecialArgs = {inherit inputs;};
-    users = {
-      "lisan" = import ./home.nix;
-    };
-  };
+  networking.networkmanager.enable = true;
 
   programs.firefox.enable = true;
   nixpkgs.config.allowUnfree = true;
@@ -74,7 +78,35 @@
     sl
     git
     fzf
+    rofi-wayland
+    dunst
+    kitty
+    libnotify
+    swww
+    lshw
+    pciutils
+
+    (waybar.overrideAttrs (oldAttrs: {
+      mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+    }))
   ];
+
+  users.users.lisan = {
+    isNormalUser = true;
+    description = "Lisan";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    ];
+  };
+
+
+  home-manager = {
+    extraSpecialArgs = {inherit inputs;};
+    users = {
+      "lisan" = import ./home.nix;
+    };
+  };
+
   nix.settings.experimental-features = [ "nix-command" "flakes"];
   
   # This value determines the NixOS release from which the default
